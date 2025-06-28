91228a0a5e2c583821d2ebc930a8fa9011ad3d8b
Michael Lehmann <lehmschi@gmail.com>
2025-06-28 22:56:28 +0200

    .

--- download/download.go
+++ download/download.go
@@ -19,7 +19,7 @@ func YoutubeVideo(url string) error {
 	downloadBinary := "yt-dlp"
 
 	output := fmt.Sprintf("%s/%%(title)s.%%(ext)s", conf.DownloadPath)
-	args := []string{"-S", "filesize~100M", "-f", "'bv+(ba[format_note*=original]/ba)'", "-o", output, url}
+	args := []string{"-S", "filesize~100M", "-f", "bv+(ba[format_note*=original]/ba)", "-o", output, url}
 	cmd := exec.Command(downloadBinary, args...)
 
 	fmt.Println(cmd.String())
