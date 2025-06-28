590ae38acf9f43b5e8b765c5d7b60e485a650183
Michael Lehmann <lehmschi@gmail.com>
2025-06-28 22:40:59 +0200

    Format Document and igrnore download errors.

--- main.go
+++ main.go
@@ -1,46 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-    "flag"
 	"kattudden/newsboat-yt/database"
 	"kattudden/newsboat-yt/download"
 )
 
-
 func main() {
 
+	newUrl := flag.String("u", "", "add url to download queue.")
+	processQueue := flag.Bool("d", false, "start downloading from queue.")
 
-    newUrl := flag.String("u", "", "add url to download queue.")
-    processQueue := flag.Bool("d", false, "start downloading from queue.")
+	flag.Parse()
 
-    flag.Parse()
+	if *newUrl != "" {
+		fmt.Println("Adding new URL.")
+		database.InsertUrl(*newUrl)
+		return
+	}
 
-    if *newUrl != "" {
-        fmt.Println("Adding new URL.")
-        database.InsertUrl(*newUrl)
-        return
-    }
+	if *processQueue {
+		fmt.Println("Processing queue...")
 
-    if *processQueue {
-        fmt.Println("Processing queue...")
+		urls := database.GetUrls()
+		for _, url := range urls {
+			err := download.YoutubeVideo(url.Link)
+			if err != nil {
+				fmt.Println("failed to download video: ", url.Link)
+				fmt.Println("but i don't care and mark it downloaded...")
+				database.MarkUrlDownloaded(url.ID)
+			}
 
-        urls := database.GetUrls()
-        for _, url := range urls {
-            err := download.YoutubeVideo(url.Link)
-            if err != nil {
-                fmt.Println("failed to download video: ", url.Link)
-                return
-            }
+			fmt.Println("successfully downloaded: ", url.Link)
+			database.MarkUrlDownloaded(url.ID)
+		}
 
-            fmt.Println("successfully downloaded: ", url.Link)
-            database.MarkUrlDownloaded(url.ID)
-        }
+		fmt.Println("Done!")
+		return
+	}
 
-        fmt.Println("Done!")
-        return
-    }
-
-    fmt.Println("Missing Argmuent; doing nothing.")
-    return
+	fmt.Println("Missing Argmuent; doing nothing.")
 }
