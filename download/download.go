6a5bbd5264f28cf702198aab5ce57b8c45dd6d26
Michael Lehmann <lehmschi@gmail.com>
2025-06-28 22:41:18 +0200

    Add option to prefer original Language.

--- download/download.go
+++ download/download.go
@@ -8,28 +8,27 @@ import (
 	"os/exec"
 )
 
-
 func YoutubeVideo(url string) error {
-    conf, _ := config.New()
+	conf, _ := config.New()
 
-    err := utils.EnsureDirectory(conf.DownloadPath) 
-    if err != nil {
-        return errors.New("failed to create download directory!") 
-    }
+	err := utils.EnsureDirectory(conf.DownloadPath)
+	if err != nil {
+		return errors.New("failed to create download directory!")
+	}
 
-    downloadBinary := "yt-dlp"
+	downloadBinary := "yt-dlp"
 
-    output := fmt.Sprintf("%s/%%(title)s.%%(ext)s", conf.DownloadPath)
-    args := []string{"-S", "filesize~100M", "-o", output, url}
-    cmd := exec.Command(downloadBinary, args...)
+	output := fmt.Sprintf("%s/%%(title)s.%%(ext)s", conf.DownloadPath)
+	args := []string{"-S", "filesize~100M", "-f", "'bv+(ba[format_note*=original]/ba)'", "-o", output, url}
+	cmd := exec.Command(downloadBinary, args...)
 
-    fmt.Println(cmd.String())
+	fmt.Println(cmd.String())
 
-    err = cmd.Run()
-    if err != nil {
-        fmt.Println(err)
-        return errors.New("failed to download video!")
-    }
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		return errors.New("failed to download video!")
+	}
 
-    return nil
+	return nil
 }
